5e7c90380e0dcacef82191df1798f9858d5cf81c
Julien Mauroy <julien.mauroy@uha.fr>
2023-12-07 15:30:13 +0100

    feat: déplacé

--- age.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-annee = int(input("Quel âge avez-vous ? : "))
-cette_annee = datetime.now().year
-
-print(f"Votre année de naissance : {cette_annee - annee}")
--- tp2exo6.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-
-lance = randint(0, 100)
-if lance < 50:
-    print("C'est pile !")
-else:
-    print("C'est face !")
